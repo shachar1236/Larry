e9b37c5c8692ab4676fe3e03f2c4952a8c9cb39a
shachar1236 <shachar5cohen@gmail.com>
2025-04-16 14:17:51 +0300

    Update TypesBitmap.hpp

--- Project/Larry/src/ECS/TypesBitmap.hpp
+++ Project/Larry/src/ECS/TypesBitmap.hpp
@@ -1,67 +1,89 @@
 #pragma once
 #include "Log.h"
 #include <cstddef>
+#include <cstring>
 #include <functional>
-#include <utility>
+#include <strings.h>
 #include <sul/dynamic_bitset.hpp>
 
+#define BITMAP_CHUNK_SIZE 4
+
+int countSetBits(unsigned long long n) 
+{ 
+    unsigned int count = 0; 
+    while (n) 
+    { 
+      n &= (n-1) ; 
+      count++; 
+    } 
+    return count; 
+} 
+
 namespace Larry::ECS {
+    using bitset = unsigned long long;
+
+    inline bitset bit(int i) {
+        return (bitset)1 << i;
+    }
 
     class TypesBitmap {
+        // TODO: currenty support up to 64 different types, make it support up to 256
         public:
-            static const int MAX_TYPE_NUMBER = 256;
-            sul::dynamic_bitset<> bitmap;
+            bitset bitmap;
+            static const int MAX_TYPE_NUMBER = sizeof(bitset) * 8 * BITMAP_CHUNK_SIZE;
 
-            TypesBitmap() : bitmap(MAX_TYPE_NUMBER) {
+            TypesBitmap() {
+                bitmap = 0;
+            }
 
+            TypesBitmap(const TypesBitmap& other) {
+                bitmap = other.bitmap;
+            }
+
+            TypesBitmap(bitset bits) {
+                bitmap = bits;
             }
 
             TypesBitmap operator|(const TypesBitmap& other) const {
-                TypesBitmap result;
-                result.bitmap = this->bitmap | other.bitmap;
-                return result;
+                return TypesBitmap(this->bitmap | other.bitmap);
             }
 
             TypesBitmap operator&(const TypesBitmap& other) const {
-                TypesBitmap result;
-                result.bitmap = this->bitmap & other.bitmap;
-                return result;
-            }
-
-            TypesBitmap operator^(const TypesBitmap& other) const {
-                TypesBitmap result;
-                result.bitmap = this->bitmap ^ other.bitmap;
-                return result;
+                return TypesBitmap(this->bitmap & other.bitmap);
             }
 
             bool operator==(const TypesBitmap& other) const {
-                return this->bitmap == other.bitmap;
+                return bitmap == other.bitmap;
             }
             
             bool IsNull() {
-                return bitmap.none();
+                return bitmap == 0;
             }
 
             static TypesBitmap TypeWithIndex(int i) {
-                TypesBitmap ret;
-                ret.bitmap.set(i);
-                return ret;
+                return TypesBitmap(bit(i));
             }
 
-            /* int FindNext(int prev) {
-                return bitmap.find_next(prev);
-            } */
+            int find_next(int prevPlusOne) {
+                for (int i = prevPlusOne; i < sizeof(bitset) * 8; i++) {
+                    if ((bitmap & bit(i)) != 0) {
+                        return i;
+                    }
+                }
+
+                return -1;
+            }
 
             inline int GetTypesCount() {
-                return bitmap.count();
+                return countSetBits(bitmap);
             }
 
             template<typename F>
             void ForEachType(const F& callback) {
-                int pos = bitmap.find_first();
-                while (pos != bitmap.npos) {
+                int pos = find_next(0);
+                while (pos != -1) {
                     callback(TypeWithIndex(pos));
-                    pos = bitmap.find_next(pos);
+                    pos = find_next(pos+1);
                 }
             }
     };
@@ -73,11 +95,6 @@ struct std::hash<Larry::ECS::TypesBitmap>
 {
     std::size_t operator()(const Larry::ECS::TypesBitmap& k) const
     {
-        std::size_t res;
-        auto data = k.bitmap.data();
-        for (int i = 0; i < k.bitmap.num_blocks(); i++) {
-            res = res ^ std::hash<unsigned long long>()(data[i]);
-        }
-        return res;
+        return std::hash<unsigned long long>()(k.bitmap);;
     }
 };
